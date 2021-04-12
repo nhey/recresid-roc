fdf780ec81e5308ec415162f4f75274832df9af8
Nikolaj Hey Hinnerskov <nhey@oehle.dk>
2021-04-12 12:06:46 +0200

    R fuckign validation

---
+++ R_validate.py
@@ -0,0 +1,64 @@
+import statsmodels.api as sm
+import numpy as np
+from python.roc import history_roc, compute_confidence_brownian, efp, sctest, history_roc_debug
+
+alpha = 0.05
+conf = compute_confidence_brownian(alpha)
+
+# R SETUP
+from rpy2.robjects.packages import importr
+import rpy2.robjects as robjects
+import rpy2.robjects.numpy2ri
+
+R = robjects.r
+rpy2.robjects.numpy2ri.activate()
+
+# History ROC from bfast package, only it returns index not `time[index]`.
+R('''
+  suppressMessages(library("bfast"))
+  history_roc.matrix <- function(X, y, level = 0.05) {
+    n <- nrow(X)
+    #data_rev <- data[n:1,]
+    #data_rev$response <- ts(data_rev$response)
+    X_rev  <- X[n:1,]
+    y_rev <-  y[n:1]
+    # time_rev <-  time[n:1]
+    
+    if (!is.ts(y)) y <- ts(y) # needed?
+    y_rcus <- efp.matrix(X_rev,y_rev, type = "Rec-CUSUM")
+    
+    pval = sctest(y_rcus)$p.value
+    y_start <- if(!is.na(pval) && pval < level) {
+      length(y_rcus$process) - min(which(abs(y_rcus$process)[-1] > boundary(y_rcus)[-1])) + 1
+    } else {
+      1    
+    }
+    y_start
+  }
+''')
+
+def load_fut_data(filename, np_dtype=np.float64, f_dtype="f64"):
+  parse = lambda line: line.replace(f_dtype,"").replace(".nan", "float('nan')")
+  with open(filename, "r") as f:
+    Xt = np.array(eval(parse(f.readline())), dtype=np_dtype)
+    image = np.array(eval(parse(f.readline())), dtype=np_dtype)
+  return (Xt, image)
+
+from glob import glob
+print("Validating data sets in ./data.")
+for fname in glob("./data/*.in"):
+  print("... ", fname)
+  Xt, image = load_fut_data(fname)
+  X = Xt.T
+  for y in image:
+    nan_inds = np.isnan(y)
+    y = y[~nan_inds]
+    Xnn = X[~nan_inds]
+    robjects.r.assign("y", y)
+    robjects.r.assign("X", Xnn)
+    py_res = history_roc_debug(Xnn.T, y, alpha, conf)
+    R_res = R("history_roc.matrix(X, y, level={})".format(alpha))
+    print("python:", py_res[0])
+    print("R:     ", int(R_res[0] - 1))
+  break
+
---
+++ r-shell.nix
@@ -0,0 +1,86 @@
+let
+  pkgs = import (fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/df8e3bd110921621d175fad88c9e67909b7cb3d3.tar.gz"
+  ) {};
+
+  futhark-version = "v0.19.3";
+  futhark-src = pkgs.applyPatches {
+    name = "futhark-patched";
+    src = (fetchTarball "https://github.com/diku-dk/futhark/archive/${futhark-version}.tar.gz");
+    patches = [ ./futhark.patch ];
+  };
+  futhark-pinned = pkgs.haskellPackages.callPackage futhark-src { suffix = "nightly"; };
+
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/870dbb751f4d851a3dfb554835a0c2f528386982.tar.gz") {};
+
+  # TODO: report bug in fetchFromGitHub? hash of other source
+  # will evaluate other source
+  strucchangeRcpp = unstable.rPackages.buildRPackage rec {
+    name = "strucchangeRcpp";
+    src = pkgs.fetchFromGitHub {
+      owner = "bfast2";
+      repo = "strucchangeRcpp";
+      rev = "1f0d4b1d5121fbee1fbe9890ee6ed572fd9ea33f";
+      sha256 = "1ayrwxwrxwl1f9l87vm252hvizpbrqb39cpyjb4yvkcdfmavmbcg";
+    };
+
+    # Remove some stripping attempt and installation of documentation
+    # that otherwise fails the build.
+    postPatch = ''
+      rm src/Makevars
+      rm -rf vignettes
+    '';
+
+    nativeBuildInputs = with unstable.rPackages; [
+      zoo
+      sandwich
+      Rcpp
+      RcppArmadillo
+    ];
+
+    propagatedBuildInputs = nativeBuildInputs;
+  };
+
+  bfast2 = unstable.rPackages.buildRPackage rec {
+    name = "bfast";
+    src = pkgs.fetchFromGitHub {
+      owner = "bfast2";
+      repo = "bfast";
+      rev = "def30feada20956c44a7f5b6c29622ae59172122";
+      sha256 = "1f6nxi0sm3s5i3prsq0cxb573pndjbc4g1pmr6p4knfiyr6dkixf";
+    };
+
+    nativeBuildInputs = with unstable.rPackages; [
+      strucchangeRcpp
+      zoo
+      forecast
+      sp
+      raster
+      stlplus
+      Rcpp
+    ];
+
+    propagatedBuildInputs = nativeBuildInputs;
+  };
+
+  Rpkgs = [
+    strucchangeRcpp
+    bfast2
+  ];
+in
+pkgs.stdenv.mkDerivation {
+  name = "shell";
+    buildInputs = with pkgs; [
+    opencl-headers
+    ocl-icd
+    futhark-pinned
+    gcc
+    (unstable.rWrapper.override { packages = Rpkgs; })
+    (unstable.python38.withPackages (pypkgs: with pypkgs; [
+      (rpy2.override { extraRPackages = Rpkgs; })
+      numpy
+      pyopencl
+      statsmodels
+    ]))
+  ];
+} 
