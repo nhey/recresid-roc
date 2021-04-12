92625e27a2b25b319160b1adcfbccebc0ba69bc3
Nikolaj Hey Hinnerskov <nhey@oehle.dk>
2021-04-12 15:42:45 +0200

    Add datasets with nans to recresid

--- recresid_validate.py
+++ recresid_validate.py
@@ -1,5 +1,6 @@
 import statsmodels.api as sm
 import numpy as np
+from load_dataset import load_fut_data
 from python.recresid import recresid
 from recresid_pyopencl import recresid_pyopencl
 
@@ -36,10 +37,31 @@ def validate(num_tests, bsz):
       break
   return ok
 
-runs = 100
-print("\nValidating {} random runs block size 1...".format(runs))
-print(validate(runs, 1))
-print("Validating {} random runs block size 2...".format(runs))
-print(validate(runs, 2))
-print("Validating {} random runs block size 4...".format(runs))
-print(validate(runs, 4))
+# runs = 100
+# print("\nValidating {} random runs block size 1...".format(runs))
+# print(validate(runs, 1))
+# print("Validating {} random runs block size 2...".format(runs))
+# print(validate(runs, 2))
+# print("Validating {} random runs block size 4...".format(runs))
+# print(validate(runs, 4))
+
+from glob import glob
+print("Validating data sets in ./data.")
+for fname in glob("./data/*.in"):
+  print("... ", fname)
+  Xt, image = load_fut_data(fname)
+  X = Xt.T
+  ok = True
+  for y in image:
+    nan_inds = np.isnan(y)
+    ynn = y[~nan_inds]
+    Xnn = X[~nan_inds]
+    py_res = recresid(Xnn, ynn)
+    ocl_res = recresid_fut.recresid(1, Xnn, ynn)
+
+    ok = ok and np.allclose(py_res, ocl_res.get())
+    if not ok:
+      print("python", py_res)
+      print("opencl", ocl_res)
+      break
+  print(ok)
